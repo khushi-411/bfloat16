8cc652b9578525f6b556b1928f77e9d5399741a0
Khushi Agrawal <khushiagrawal411@gmail.com>
2024-04-11 16:14:33 +0530

    fix

--- main.cpp
+++ main.cpp
@@ -9,20 +9,17 @@ int main() {
     // c10::BFloat16 b(3.14f);
     float b = 2.0;
 
-    float f1 = static_cast<float>(a);
-    float f2 = static_cast<float>(b);
-
     c10::BFloat16 c = a + b;
     c10::BFloat16 d = a - b;
     c10::BFloat16 e = a * b;
     c10::BFloat16 f = a / b;
 
-    std::cout << "a = " << a.x << ", value = " << f1 << "\n";
-    std::cout << "b = " << b << ", value = " << f2 << "\n";
-    std::cout << "c = " << c.x << ", value = " << c << "\n";
-    std::cout << "d = " << d.x << ", value = " << d << "\n";
-    std::cout << "e = " << e.x << ", value = " << e << "\n";
-    std::cout << "f = " << f.x << ", value = " << f << "\n";
+    std::cout << "a.x = " << a.x << "; a = " << a << "\n";
+    std::cout << "b.x = " << b << "; b = " << b << "\n";
+    std::cout << "c.x = " << c.x << "; c = " << c << "\n";
+    std::cout << "d.x = " << d.x << "; d = " << d << "\n";
+    std::cout << "e.x = " << e.x << "; e = " << e << "\n";
+    std::cout << "f.x = " << f.x << "; f = " << f << "\n";
 
     return 0;
 }
